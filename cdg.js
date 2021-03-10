1ce469b33643ae62caf0c1afd147f2eeee0d909b
smozzington <80350893+smozzington@users.noreply.github.com>
2021-03-10 14:42:29 +0000

    Add files via upload

---
+++ cdg.js
@@ -0,0 +1,40 @@
+var cardSlot;
+
+document.getElementById("small_number_card").onclick = function(){
+    genSmlNumber();
+}
+
+
+
+
+
+//Generate small number in next available card slot
+function genSmlNumber(){
+    var smlNumber = Array(1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10);
+    var x = smlNumber[Math.floor(Math.random()*smlNumber.length)];
+        var cardSlot = 1;
+        document.getElementById("box"+cardSlot).innerHTML = x;
+        for(i=1; i<=6; i++){
+            if(cardSlot == x){
+            document.getElementById("box"+i).innerHTML = x;
+        }
+    }
+}
+
+        
+        
+        
+        
+        
+//        var cardSlot = 1;
+//        document.getElementById("box"+cardSlot).innerHTML = x;
+//        var slotFull = [x];
+//        if(){
+//    
+//        }
+//    }
+//}
+
+
+
+//document.getElementById("box"+i).innerHTML = x;
